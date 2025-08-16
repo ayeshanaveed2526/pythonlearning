dabab350e9d0b9b36cc7798506e9d80ad5755d9a
Ayesha Naveed <hr34772@gmail.com>
2025-08-16 15:27:26 +0500

    changing

--- texttospeech.py
+++ texttospeech.py
@@ -1,4 +1,4 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("i my name is habib and i am mota alu and some people call me suzi")
+engine.say("my name is ayesha and i am in love with hassan")
 engine.runAndWait()
