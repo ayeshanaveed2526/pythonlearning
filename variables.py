ca16e08b8f9c6f3c52236a105a7433be1afc7bf9
Ayesha Naveed <hr34772@gmail.com>
2025-08-17 15:37:33 +0500

    boolean

--- variables.py
+++ variables.py
@@ -7,8 +7,14 @@ print(a - b)
 print(a * b)
 print(a / b)
 # Assignent operators
-a= 4+8
+a += 4
 b=6-3
 c=2*3
 d=8/2
 print(a,b,c,d)
+# boolean operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
