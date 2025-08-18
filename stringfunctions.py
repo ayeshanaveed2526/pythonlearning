e65dd859a4843256c94e91804385544a36cbd89a
Ayesha Naveed <hr34772@gmail.com>
2025-08-18 22:33:50 +0500

    functions of strings

---
+++ stringfunctions.py
@@ -0,0 +1,19 @@
+#Some Strings Functions
+#String Length
+name= "ayeshanaveed"
+print(len(name))
+#Strings ends with, function
+print(name.endswith("eed"))
+#tell if is it true or not
+#Strings starts with, function
+print(name.startswith("aya"))
+#For capital letter
+print(name.capitalize())
+#for lower letter
+print(name.lower())
+#for upper letter
+print(name.upper())
+# for finding a string
+print(name.find("sha"))
+#for count any alphabet
+print(name.count("a"))
