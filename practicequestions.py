32d7980cf59a4334820663254b99efc09d092e91
Ayesha Naveed <hr34772@gmail.com>
2025-08-18 17:56:52 +0500

    average of two values

--- practicequestions.py
+++ practicequestions.py
@@ -5,6 +5,11 @@
 #     print("a is greater than b")
 # else:
 #     print("a is not greater than b")
-a= int(input("enter a:"))
-b= int(input("enter b:"))
-print("your  values are:", a>b)
+#another method
+# a= int(input("enter a:"))
+# b= int(input("enter b:"))
+# print("your  values are:", a>b)
+#2-write a program to found average of two number entered by user
+a= int(input("enter no 1:"))
+b=int(input("enter no 2:"))
+print("the average of two values are",  a+b/2)
