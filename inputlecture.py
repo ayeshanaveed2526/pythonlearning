180c1e8996638e08141a7116185fff7681675e8c
Ayesha Naveed <hr34772@gmail.com>
2025-08-18 17:48:34 +0500

    comaprison operator

--- inputlecture.py
+++ inputlecture.py
@@ -1,5 +1,8 @@
-a = input("enter no 1")
-b = input("enter no 2")
-print("no 1 is =", a)
-print("no 2 is =",b)
-print("sum is =", int(a) + int(b))
+# a = input("enter no 1")
+# b = input("enter no 2")
+# print("no 1 is =", a)
+# print("no 2 is =",b)
+# print("sum is =", int(a) + int(b))
+a=24
+b=5
+print(" Remainder is ", a%b)
---
+++ practicequestions.py
@@ -0,0 +1,7 @@
+# make a program for use comparison operator to find out whether a given variable a is greater then b or not take a = 34 b=80
+# a=34
+# b=80
+# if (a>b):
+#     print("a is greater than b")
+# else:
+#     print("a is not greater than b")
