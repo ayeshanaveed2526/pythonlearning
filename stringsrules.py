176901729d63970c4904f2b016690ab652b11004
Ayesha Naveed <hr34772@gmail.com>
2025-08-18 18:28:32 +0500

    strings

---
+++ stringsrules.py
@@ -0,0 +1,13 @@
+#STRINGS RULES
+#three ways to write strings
+a= "ayesha"
+b='rafia'
+c='''habib'''
+print(a,b,c)
+#String slicing
+# A string is immutable, if a string is created, it cannot be changed.
+name ="ayeshanaveed"
+nameshort=name[0:6] #start from 0 and include all values from 1to 3 and excluded values of 4 to onwards
+print(nameshort)
+namelength=len(name) #get the length of the string
+print(namelength)
