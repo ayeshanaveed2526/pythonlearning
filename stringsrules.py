dac869b71e790adcd7ca353671815bf182ef52cb
Ayesha Naveed <hr34772@gmail.com>
2025-08-18 22:13:22 +0500

    more rules

--- stringsrules.py
+++ stringsrules.py
@@ -1,16 +1,23 @@
-#STRINGS RULES
-#three ways to write strings
-a= "ayesha"
-b='rafia'
-c='''habib'''
-print(a,b,c)
-#String slicing
-# A string is immutable, if a string is created, it cannot be changed.
-name ="ayeshanaveed"
-nameshort=name[0:6] #start from 0 and include all values from 1to 3 and excluded values of 4 to onwards
-print(nameshort)
-namelength=len(name) #get the length of the string
-print(namelength)
-#we can print a letter seperately
-char=name[3]
-print(char)
+# #STRINGS RULES
+# #three ways to write strings
+# a= "ayesha"
+# b='rafia'
+# c='''habib'''
+# print(a,b,c)
+
+# #String slicing
+# # A string is immutable, if a string is created, it cannot be changed.
+# name ="ayeshanaveed"
+
+# nameshort=name[0:6] #start from 0 and include all values from 1to 3 and excluded values of 4 to onwards
+# print(nameshort)
+
+# namelength=len(name) #get the length of the string
+# print(namelength)
+# #we can print a letter seperately
+# char=name[3]
+# print(char)
+#slicing with skiping words
+# name= "habib"
+# slice= name[0: 4: 2]
+# print(slice)
