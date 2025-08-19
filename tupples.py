b76586e0066fe68bd356e46aa7d311ad3914dc85
Ayesha Naveed <hr34772@gmail.com>
2025-08-19 12:00:54 +0500

    tuples complete

--- tupples.py
+++ tupples.py
@@ -11,3 +11,41 @@ a=(1,)
 print(type(a))
 print(a)
 #we cant change tuple because they are immutable and they cannot be modified after creation.
+#Tuple methods
+#TUPPLE PRACTICE
+#write a program to store 7 fruits in a list entered by the user
+# fruits=[]
+# fruit1=input("Enter fruit 1: ")
+# fruit2=input("Enter fruit 2: ")
+# fruit3=input("Enter fruit 3: ")
+# fruit4=input("Enter fruit 4: ")
+# fruit5=input("Enter fruit 5: ")
+# fruit6=input("Enter fruit 6: ")
+# fruit7=input("Enter fruit 7: ")
+# fruits.append(fruit1)
+# fruits.append(fruit2)
+# fruits.append(fruit3)
+# fruits.append(fruit4)
+# fruits.append(fruit5)
+# fruits.append(fruit6)
+# fruits.append(fruit7)
+# print(fruits)
+#problem no 2
+#write a program to accept marks of 5 students and show them in sorted manner
+# marks=[]
+# student1=input("Enter marks of student 1: ")
+# student2=input("Enter marks of student 2: ")
+# student3=input("Enter marks of student 3: ")
+# student4=input("Enter marks of student 4: ")
+# student5=input("Enter marks of student 5: ")
+# marks.append(student1)
+# marks.append(student2)
+# marks.append(student3)
+# marks.append(student4)
+# marks.append(student5)
+# marks.sort()
+# print(marks)
+#problem no 3
+# check that tuple type that cannot be changed in python
+# a=(8,4,3,"ayesha")
+# a[3]="rafia"
