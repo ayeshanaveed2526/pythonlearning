4f05cefc65ee791fc85423fe96c1e4425e48c04a
Ayesha Naveed <hr34772@gmail.com>
2025-08-20 15:38:39 +0500

    sets

--- sets.py
+++ sets.py
@@ -4,3 +4,14 @@ e =set()  # empty set
 s ={1,3,4,56,7,4,7,8,8,2,6}
 print(s)
 print(type(s))  # <class 'set'>
+#set method
+s.add(10)  # add element
+print(s)
+s.remove(7)  # remove element
+print(s)
+s.update([1,2,3])  # add multiple elements
+print(s)
+y={4,5,6}
+print(s.union(y))  # union of two sets
+print(s.intersection(y))  # intersection of two sets
+print(s.difference(y))  # difference of two sets
