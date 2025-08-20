291011d4b96717d0edaf4a3a25e44d42d6a1ffb3
Ayesha Naveed <hr34772@gmail.com>
2025-08-20 16:37:48 +0500

    if else

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+#conditional statements
+age =int(input("Enter your age:"))
+if (age>=18):
+    print("you are elgible for driving")
+else:
+    print("you are not eligible for driving")
+    print("Please try again when you are older.")
