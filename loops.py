06e7a6f7cef2db2b1273cc78cb86963013e39c5b
Ayesha Naveed <hr34772@gmail.com>
2025-08-20 19:29:30 +0500

    loo

---
+++ loops.py
@@ -0,0 +1,12 @@
+#while loops: jab tk condition true nahi hoti tb tk loop chalta hai
+
+# i=1
+# while(i<11):
+#     print(i)
+#     i+=1
+
+l=["ayesha",1,5,3,False,True,"ali","hasan"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
