da9969c498f6292f3cc3201d80805047eca65cba
Ayesha Naveed <hr34772@gmail.com>
2025-10-21 01:16:49 +0500

    quiz 2,3 and 4

--- quizbeg.py
+++ quizbeg.py
@@ -1,8 +1,15 @@
 # Write a program that takes an integer and prints whether it’s even or odd.
-a= int(input("Enter your number"))
-if a%2==0:
-    print(a, "your number is even")
+# a= int(input("Enter your number"))
+# if a%2==0:
+#     print(a, "your number is even")
 
-else:
-    print (a, "your number is odd")
-    
+# else:
+#     print (a, "your number is odd")
+# Given a list of numbers, calculate their total using a for loop and also using Python’s built-in sum().
+a =[1,4,5,6,4,0,5,3,2,5,7,8]
+i=0
+for num in a:
+    i+=num
+    print("Total using for loop is ", i)
+    #and after using su function
+    print("Total using sum function is ", sum(a))
